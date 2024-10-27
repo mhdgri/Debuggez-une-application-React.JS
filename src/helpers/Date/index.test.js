1b6fb3d94e6cf98a91bc3e48ee1997f952a9d81b
Mohamed Guerroui <mohamed.guerroui.mg@gmail.com>
2024-10-27 02:26:42 +0100

    Ajout des tests pour vérifier que les bons mois sont retournés

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -1,15 +1,19 @@
-/**
- * 
- */
+import { getMonth } from "./index";
+
 
 describe("Date helper", () => {
     describe("When getMonth is called", () => {
-        it("the function return janvier for 2022-01-01 as date", () => {
-            // to implement
-        });
-        it("the function return juillet for 2022-07-08 as date", () => {
-            // to implement
-        });
+      it("the function returns janvier for 2022-01-01 as date", () => {
+        const date = new Date("2022-01-01"); // Crée une instance de Date pour le 1er janvier 2022
+        const month = getMonth(date); // Appelle la fonction getMonth
+        expect(month).toBe("janvier"); // Vérifie que le mois retourné est "janvier"
+      });
+  
+      it("the function returns juillet for 2022-07-08 as date", () => {
+        const date = new Date("2022-07-08"); // Crée une instance de Date pour le 8 juillet 2022
+        const month = getMonth(date); // Appelle la fonction getMonth
+        expect(month).toBe("juillet"); // Vérifie que le mois retourné est "juillet"
+      });
     });
-})
+  });
 
