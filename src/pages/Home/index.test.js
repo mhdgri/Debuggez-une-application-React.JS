4a33e8267f0d86c1b61a31dab10597a20d4d9c19
Mohamed Guerroui <mohamed.guerroui.mg@gmail.com>
2024-12-08 01:27:32 +0100

    Test d'affichage des éléments de la page 'Home'

--- src/pages/Home/index.test.js
+++ src/pages/Home/index.test.js
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import Home from "./index";
 
 describe("When Form is created", () => {
@@ -20,8 +20,9 @@ describe("When Form is created", () => {
           bubbles: true,
         })
       );
-      await screen.findByText("En cours");
-      await screen.findByText("Message envoyé !");
+      await waitFor(() => {
+        expect(screen.findByText("Message envoyé !")).resolves.toBeInTheDocument();
+      }) 
     });
   });
 
@@ -29,16 +30,31 @@ describe("When Form is created", () => {
 
 
 describe("When a page is created", () => {
-  it("a list of events is displayed", () => {
-    // to implement
-  })
-  it("a list a people is displayed", () => {
-    // to implement
-  })
-  it("a footer is displayed", () => {
-    // to implement
-  })
-  it("an event card, with the last event, is displayed", () => {
-    // to implement
+  it("a list of events is displayed", async () => {
+    render(<Home />);
+    const eventList = await screen.findByTestId("events");
+    expect(eventList).toBeInTheDocument();
+  });  
+
+  it("a list of people is displayed", async () => {
+    render(<Home />);
+    const cardImages = await screen.findAllByTestId("card-image-testid");
+    expect(cardImages.length).toBeGreaterThan(0);
+  });
+
+
+  it("a footer is displayed", async () => {
+    render(<Home />);
+    const footer = await screen.findByTestId("footer");
+    expect(footer).toBeInTheDocument();
+  });
+
+
+  it("an event card, with the last event, is displayed",   () => {
+    render(<Home />);
+    waitFor(() => {
+      const cardDisplayed = screen.getByTestId("card-testid");
+      expect(cardDisplayed).toBeInTheDocument();
+    });
   })
 });
