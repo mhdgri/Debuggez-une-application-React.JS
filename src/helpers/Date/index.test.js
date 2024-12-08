8581c2e31b7d238872834a4cf73609b935fa0f0c
Mohamed Guerroui <mohamed.guerroui.mg@gmail.com>
2024-12-08 01:30:55 +0100

    tests unitaire pour afficher les bon mois de janvier et de juillet

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -4,16 +4,15 @@ import { getMonth } from "./index";
 describe("Date helper", () => {
     describe("When getMonth is called", () => {
       it("the function returns janvier for 2022-01-01 as date", () => {
-        const date = new Date("2022-01-01"); // Crée une instance de Date pour le 1er janvier 2022
-        const month = getMonth(date); // Appelle la fonction getMonth
-        expect(month).toBe("janvier"); // Vérifie que le mois retourné est "janvier"
+        const date = new Date("2022-01-01");
+        const month = getMonth(date);
+        expect(month).toBe("janvier");
       });
   
       it("the function returns juillet for 2022-07-08 as date", () => {
-        const date = new Date("2022-07-08"); // Crée une instance de Date pour le 8 juillet 2022
-        const month = getMonth(date); // Appelle la fonction getMonth
-        expect(month).toBe("juillet"); // Vérifie que le mois retourné est "juillet"
+        const date = new Date("2022-07-08");
+        const month = getMonth(date);
+        expect(month).toBe("juillet");
       });
     });
   });
-
